97278833b7d1b2e060b677c877658c1aae7e8bc9
Meek Meek <haggins.jameka@gmail.com>
2026-02-27 11:34:17 -0500

    I'm not that great with localStorage, but I learned that async await is not the way to go for handling that data the hard way.

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error("Could not fetch the data.");
+
+        const result = await response.json();
+        setData(result);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+  return { data, loading, error};
+};
+
+export default useFetch;
--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -1,9 +1,23 @@
+import useFetch from '../hooks/useFetch';
+
 function RecipesPage() {
+  const { data, loading, error } = useFetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
+  if (loading) return <p>Yummy recipes are on their way...</p>
+  if (error) return <p>Error: {error}</p>;
+  
   return (
     <div>
-      This is a recipes page
+      <h1>Seafood Recipes</h1>
+      <div className="recipe-grid">
+        {data.meals.map((meal) => (
+          <div key={meal.idMeal} className="recipe-card">
+            <img src={meal.strMealThumb} alt={meal.strMeal} width="200" />
+            <h3>{meal.strMeal}</h3>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default RecipesPage;
