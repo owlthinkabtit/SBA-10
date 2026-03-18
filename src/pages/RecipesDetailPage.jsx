d5f16fb9fb81d15bd9b23e93cc4c7ac39f8e36a6
Meek Meek <haggins.jameka@gmail.com>
2026-03-18 15:52:26 -0400

    Looks like the change I made with the recipes page isn't working. Will figure out what actually broke. Think it was because I was trying to be fancy and make the formatting of the text look like less of a wall of text.

--- src/pages/RecipesDetailPage.jsx
+++ src/pages/RecipesDetailPage.jsx
@@ -1,34 +1,49 @@
 import { useFavorites } from "../context/FavoritesContext";
-import { useParams } from 'react-router-dom';
-import useFetch from '../hooks/useFetch';
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 import Spinner from "../components/Spinner";
 
-
-
 function RecipesDetailPage() {
-  const {recipeId } = useParams();
+  const { recipeId } = useParams();
   const { addFavorite, removeFavorite, isFavorite } = useFavorites();
   const heartClicked = isFavorite(recipeId);
-  const { data, loading, error } = useFetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
+  const { data, loading, error } = useFetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`,
+  );
+  const meal = data.meals[0];
+  const instructionsSteps = meal.strInstructions.split(".").filter((step) => step.trim().length > 0);
 
   if (loading) return <Spinner />;
-  if (error) return <p>Error: {error}</p>
+  if (error) return <p>Error: {error}</p>;
 
-  const meal = data.meals[0];
 
   return (
     <div className="recipe-detail">
       <h1>{meal.strMeal}</h1>
-      <img src={meal.strMealThumb} alt={meal.strMeal} style={{ width: '300px'}} />
+      <img
+        src={meal.strMealThumb}
+        alt={meal.strMeal}
+        style={{ width: "100px" }}
+      />
+      <h3>Instructions</h3>
+      <ol>
+        {instructionsSteps.map((step, index) => (
+          <li key={index} style={{ marginBottom: "10px" }}>
+            {step.trim()}.
+          </li>
+        ))}
+      </ol>
       <div>
-        <button onClick={() => heartClicked ? removeFavorite(recipeId) : addFavorite(recipeId)}>
+        <button
+          onClick={() =>
+            heartClicked ? removeFavorite(recipeId) : addFavorite(recipeId)
+          }
+        >
           {heartClicked ? "❤️ Remove from Favorites" : "🤍 Add to Favorites"}
         </button>
       </div>
-      <h3>Instructions</h3>
-      <p>{meal.strInstructions}</p>
     </div>
-  )
+  );
 }
 
 export default RecipesDetailPage;
